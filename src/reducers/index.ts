6cba67b1ba9487cb49b2bb28107d68e14facbd87
哲峰  邹 <zhefengzou@zhefengs-MacBook-Pro.local>
2021-10-26 00:08:12 +0800

    first commit

---
+++ src/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import { todosReducers } from "./todos";
+import { Todo } from '../actions';
+
+export interface StoreState {
+    todos: Todo[]
+}
+
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducers
+});
